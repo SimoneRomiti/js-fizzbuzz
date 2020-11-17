2587080509a2ca00e92758ebc5da72feb2e1670a
Simone Romiti <sr1992ei@gmail.com>
2020-11-17 18:51:23 +0100

    Esercizio definitivo

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
+// PULSANTE GENERA
+document.getElementById("button").innerHTML = "GENERA";
 var pulsante = document.getElementById("button");
 var numero;
+
+// PULSANTE RESET
+document.getElementById("delete").innerHTML = "RESET";
+var reset = document.getElementById("delete");
+
+// FUNZIONE PULSANTE GENERA
 pulsante.addEventListener("click",
 function(){
+
+  // CICLO FOR PER TROVARE MULTIPLI
   for(var i = 1; i <= 100; i++){
     numero = i;
     if(i % 3 == 0 && i % 5 == 0){
@@ -16,6 +26,7 @@ function(){
       numero = "Buzz";
     }
 
+    // SCRITTURA NUMERI IN <LI> E IF PER CAMBIO COLORE MULTIPLI
     if(numero == "Fizz"){
       document.getElementById("elenco").innerHTML += '<li class="flex orange">' + numero + '</li>';
     } else if(numero == "Buzz"){
@@ -26,9 +37,44 @@ function(){
       document.getElementById("elenco").innerHTML += '<li class="flex">' + numero + '</li>';
     }
 
-    console.log(numero);
   }
+  // FINE CICLO FOR
+
+  // EFFETTO APPARIZIONE CON ANIMATION 1 SEC SU CSS
+  document.getElementById("visible").className = "show";
+  // SECONDO CLICK DISABILITATO E OPACITY 0.5 AL PULSANTE
+  pulsante.disabled = true;
+  pulsante.className = "my-btn-V2";
+
+  // PULSANTE RESET DI NUOVO CLICCABILE CON RITARDO DI 1 SEC PER FAR FINIRE ANIMAZIONE APPARIZIONE
+  setTimeout(function enable(){
+    reset.disabled = false;
+    reset.className = "my-btn";
+  }, 1000);
 
 
 }
 );
+// FINE FUNZIONE PULSANTE GENERA
+
+
+// FUNZIONE PULSANTE RESET
+reset.addEventListener("click",
+function(){
+  // EFFETTO SCOMPARSA CON ANIMATION 1 SEC SU CSS
+  document.getElementById("visible").className = "hidden";
+
+  // ELIMINAZIONE CONTENUTI DELLA LISTA E PULSANTE GENERA DI NUOVO CLICCABILE CON RITARDO DI 1 SEC PER FAR FINIRE ANIMAZIONE SCOMPARSA
+  setTimeout(function cancella(){
+    document.getElementById("elenco").innerHTML = "";
+
+    pulsante.disabled = false;
+    pulsante.className = "my-btn";
+  }, 1000);
+
+  reset.disabled = true;
+  reset.className = "my-btn-V2";
+
+}
+);
+// FINE FUNZIONE PULSANTE RESET
